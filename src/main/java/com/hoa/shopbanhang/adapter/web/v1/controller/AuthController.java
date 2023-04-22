eb3142d4a634b2536467b06e6de5a693790d9980
nguyenhoa25 <nguyenhoa25092001@gmail.com>
2023-04-22 23:44:51 +0700

    [code] authorization user and admin

--- src/main/java/com/hoa/shopbanhang/adapter/web/v1/controller/AuthController.java
+++ src/main/java/com/hoa/shopbanhang/adapter/web/v1/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.hoa.shopbanhang.adapter.web.v1.transfer.parameter.auth.Authentication
 import com.hoa.shopbanhang.application.constants.UrlConstant;
 import com.hoa.shopbanhang.application.inputs.user.CreateUserInput;
 import com.hoa.shopbanhang.application.services.IAuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,17 +24,20 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Log In")
     @PostMapping(UrlConstant.Auth.LOGIN)
     public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest) {
         return VsResponseUtil.ok(authService.login(authenticationRequest));
     }
 
+    @Operation(summary = "Sign Up")
     @PostMapping (UrlConstant.Auth.SIGNUP)
     public ResponseEntity<?> signUp(@Valid @RequestBody CreateUserInput createUserInput,
                                     HttpServletRequest request) {
         return VsResponseUtil.ok(authService.signUp(createUserInput, request));
     }
 
+    @Operation(summary = "Refresh Token")
     @PostMapping(UrlConstant.Auth.REFRESH_TOKEN)
     public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) {
         return ResponseEntity.ok().body(authService.refreshToken(request, response));
